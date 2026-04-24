fababb4bc895725ce108871d2d1ad72a0a430724
Mari <mariane_didomenico@estudante.sesisenai.org.br>
2026-04-24 11:37:55 +0000

    update

--- listapython/atividade.27.py
+++ listapython/atividade.27.py
@@ -1,2 +1,6 @@
 numero1 = int (input("Escreva um número"))
 numero2 = int (input("Escreva um segundo número"))
+
+div = numero1/numero2
+
+print("A divisão é: ",div)
---
+++ listapython/atividade29.py
@@ -0,0 +1,6 @@
+preco_original = float(input("Digite o preço do produto: R$ "))
+
+novo_preco = preco_original * 0.90
+
+print("O preço com 10% de desconto é: ",novo_preco)
+
